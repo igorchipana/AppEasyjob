8e380e4e2fefc8a43018c323dc3611bad08f80e3
danilo <arnoldmorales@upeu.edu.pe>
2018-05-11 23:15:22 -0500

    modificaciones

---
+++ app/src/main/java/com/example/igor/msqlandroid/mundoload.java
@@ -0,0 +1,13 @@
+package com.example.igor.msqlandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mundoload extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mundoload);
+    }
+}
